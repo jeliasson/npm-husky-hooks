8ecb5fd520add0fcf4b7c417a8675603997b4667
Johan Eliasson <jeliasson@users.noreply.github.com>
2022-06-03 15:47:18 +0200

    fix(hook): handle empty command

--- src/hooks/runCmd.ts
+++ src/hooks/runCmd.ts
@@ -1,10 +1,20 @@
 import { execSync } from 'child_process'
 
 import { ThrowError, useHookResponse } from '../cli/response'
+import { CONFIG_FILE } from '../config'
 import { HookResponse } from '../hooks/index.types'
 
 export async function runCmd(command: string): Promise<HookResponse> {
   const { stdout, errors } = useHookResponse()
+
+  if (!command)
+    ThrowError([
+      `Command to run is empty.`,
+      `It's probabaly a typo in the hooks section of the ${CONFIG_FILE} config file.`,
+      '',
+      'Example:',
+      "['run-cmd', 'yarn lint']",
+    ])
 
   try {
     const output = execSync(command, {
