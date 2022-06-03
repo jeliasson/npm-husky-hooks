b294490954433feb7cd673b40a4a436650efbd15
Johan Eliasson <jeliasson@users.noreply.github.com>
2022-06-03 14:19:17 +0200

    feat(repo): adding husky-hook config (#22)

---
+++ husky-hooks.config.js
@@ -0,0 +1,59 @@
+const config = {
+  hooks: {
+    'pre-commit': [
+      ['run-cmd', 'yarn lint'],
+      ['run-cmd', 'yarn format'],
+      'check-branch',
+      'check-lock-files',
+    ],
+
+    'pre-push': [
+      ['run-cmd', 'tsc'],
+      ['run-cmd', 'yarn lint'],
+      'check-branch',
+      'check-lock-files',
+    ],
+  },
+
+  settings: {
+    /**
+     * check-branch
+     *
+     * Check which git branch we're currently on, and abort if it's a
+     * protected branch. This can be useful to make sure that commits stay
+     * away from branches that only being used for Pull Requests or CI/CD.
+     */
+    'check-branch': {
+      // Git branches that should be protected from accidental commit or push
+      protectedBranches: ['main', 'dev'],
+    },
+
+    /**
+     * check-lock-files
+     *
+     * Check for package manager lock files, and abort if any are present
+     * lock file that we don't want. This is useful to ensure that e.g.
+     * only yarn.lock is present in the repository.
+     */
+    'check-lock-files': {
+      // Package manager lock file that should be present in the repository
+      allowLockFile: 'yarn.lock',
+
+      // Package manager lock files that should yield a abort
+      denyLockFiles: ['package-lock.json', 'pnpm-lock.yaml'],
+    },
+
+    /**
+     * test-sleep
+     *
+     * This is a dummy hook for demo purposes. It sleeps for 1000 seconds
+     * before continuing to next hook.
+     */
+    'test-sleep': {
+      // How long should the hook sleep for
+      delay: 1000,
+    },
+  },
+}
+
+module.exports = config
