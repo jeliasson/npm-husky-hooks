e43b0ed963117c7b03a3e95a57df8438644915ba
Johan Eliasson <jeliasson@users.noreply.github.com>
2022-06-05 15:17:45 +0200

    cleanup(repo): general refactoring

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,32 @@
+import { PACKAGE_NAME } from '../config'
 import { createConfigCommand } from './createConfig'
 import { Commands } from './index.types'
 
-export const events = ['pre-commit', 'pre-push']
-
+// Registered commands
 export const commands = <Commands>{
   'create-config': createConfigCommand,
+}
+
+/**
+ * Run command
+ *
+ * @param   {string} name               Name of command to run
+ * @returns {Promise<void>}
+ */
+export async function runCommand(name: string): Promise<void> {
+  // @todo: Refactor into runCommand maybe? Probabaly.
+  if (!name) {
+    console.log(`\n❌ Missing command, e.g. create-config.\n`)
+    console.log(`> npx ${PACKAGE_NAME} create-config`)
+
+    process.exit(1)
+  }
+
+  const command = commands[name]
+  if (typeof command === 'function') {
+    await command()
+
+    // Exit out from here
+    process.exit(0)
+  }
 }
