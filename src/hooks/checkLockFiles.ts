1a0d996bbdad1ebb3694e1b0302b219bc8ffb4d7
Johan Eliasson <jeliasson@users.noreply.github.com>
2022-09-24 17:36:22 +0200

    fix(hook): fixing check-lock-files

--- src/hooks/checkLockFiles.ts
+++ src/hooks/checkLockFiles.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 
+import { getConfigSettingByName } from '../config'
 import { HookResponse } from '../types/hooks'
 
 import { useHookResponse } from '../cli/response'
@@ -7,11 +8,20 @@ import { useHookResponse } from '../cli/response'
 export async function checkLockFiles(): Promise<HookResponse> {
   const { stdout, errors } = useHookResponse()
 
-  const allowLockFile = 'yarn.lock'
-  const denyLockFiles = ['package-lock.json', 'pnpm-lock.yaml']
+  // Allowed lock file
+  const allowLockFile = await getConfigSettingByName(
+    'check-lock-files',
+    'allowLockFile'
+  )
+
+  // Deny lock files
+  const denyLockFiles = await getConfigSettingByName(
+    'check-lock-files',
+    'denyLockFiles'
+  )
 
   // Check files
-  for (const file of denyLockFiles) {
+  for (const file of denyLockFiles.value) {
     if (fs.existsSync(file)) {
       errors.push(
         `Invalid occurence of "${file}" file. Please remove it and only use "${allowLockFile}"`
@@ -20,7 +30,7 @@ export async function checkLockFiles(): Promise<HookResponse> {
   }
 
   try {
-    const content = fs.readFileSync(allowLockFile, 'utf-8')
+    const content = fs.readFileSync(allowLockFile.value, 'utf-8')
     if (content.match(/localhost:487/)) {
       errors.push(
         `The "${allowLockFile}" has reference to local yarn repository ("localhost:4873"). Please use "registry.yarnpkg.com" in "${allowLockFile}"`
